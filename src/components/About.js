2e773bb9475d8b0fbe7264785132b9ae4596f9ea
vishal singh <vishalsinghbadm@gmail.com>
2022-05-29 11:45:04 +0530

    final commit

--- src/components/About.js
+++ src/components/About.js
@@ -27,22 +27,21 @@ let mystyle ={
         <strong> Ower Details</strong>
       </button>
     </h2>
-    <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+    <div id="collapseOne" className="accordion-collapse collapse 0show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
       <div className="accordion-body">
-        <h1>Anil Babu</h1>
-        <p>Email on <span></span>  <a href='anilb9850@gmail.com'>anilb9850@gmail.com</a> </p> 
+        <h1>vishal singh</h1>
+        <p>Email on <span></span>  <a href='vishalsinghbadm@gmail.com'>vishalsinghbadm@gmail.com</a> </p> 
       
         <div className= "social-icons mt-4">
-                <a href="https://www.facebook.com/profile.php?id=100041724041773" target="_blank"><i className="fab btn btn-success mx-2 my-1 bfa-facebook">Facebook</i></a>
-                <a href="https://www.instagram.com/gangwar1304/" target="_blank"><i className="fab btn btn-success mx-2 my-1 fa-instagram">Instagram</i></a>
-                <a href="https://twitter.com/AnilBab10138171?t=JbSlKFwwYdtSyZDMNkdXtw&s=08" target="_blank"><i className="fab btn btn-success mx-2 my-1 fa-twitter">twitter</i></a>
-                <a href="https://www.linkedin.com/in/anil-babu-19958a208" target="_blank"><i className="fab btn btn-success mx-2 my-1 fa-linkedin">Linkedin</i></a>
-                <a href="https://github.com/Anilpateel" target="_blank"><i className="fab btn btn-success mx-2 my-1 fa-github">Github</i></a>
+                <a href="https://www.facebook.com/vishalsinghbadm" target="_blank"><i className="fab btn btn-success mx-2 my-1 bfa-facebook">Facebook</i></a>
+                <a href="https://www.instagram.com/rajpoot_vishal_._/?hl=en" target="_blank"><i className="fab btn btn-success mx-2 my-1 fa-instagram">Instagram</i></a>
+                <a href="https://www.linkedin.com/in/vishal-singh-3360a5195/" target="_blank"><i className="fab btn btn-success mx-2 my-1 fa-linkedin">Linkedin</i></a>
+                <a href="https://github.com/vishal9534" target="_blank"><i className="fab btn btn-success mx-2 my-1 fa-github">Github</i></a>
             </div>
      </div>
     </div>
   </div>
-  <div className="accordion-item" style={mystyle} style={mystyle}>
+  <div className="accordion-item" style={mystyle}>
     <h2 className="accordion-header" id="headingTwo">
       <button className="accordion-button collapsed" style={mystyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
      <strong> Browser support </strong>
